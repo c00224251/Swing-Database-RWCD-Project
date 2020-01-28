8736e0deddc97ef78ce09564029544c2685a535f
Ezzar <Ezzar@149.153.142.101>
2020-01-28 13:53:00 +0000

    ugabuga

---
+++ Projekt Blaviken/src/gui/mainDriver.java
@@ -0,0 +1,24 @@
+package gui;
+import javax.swing.JFrame;
+
+/* notatki kurwa
+ * 
+ * tabelki w JScrollPane bo kurwa scroll bedzie
+ * https://www.javatpoint.com/java-joptionpane
+ * https://www.javatpoint.com/java-jtable
+ * https://www.javatpoint.com/java-jseparator
+ * 
+ */
+
+
+
+public class mainDriver extends JFrame
+	{
+
+		public static void main(String[] args)
+			{
+
+				new menu();
+			}
+
+	}
---
+++ Projekt Blaviken/src/gui/menu.java
@@ -0,0 +1,42 @@
+package gui;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class menu extends JFrame
+	{
+		public menu()
+			{
+				JTable table = new JTable();
+				setSize(700, 700);
+				setTitle("czrne chuje brzydko pachna");
+				setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				new BorderLayout();
+
+				JPanel tableMenu = new JPanel(new FlowLayout());
+				add(new JScrollPane(table), BorderLayout.CENTER);
+
+				add(tableMenu, BorderLayout.PAGE_START);
+				JButton customerButton = new JButton("Customer");
+				JButton productButton = new JButton("Product");
+				JButton invoiceButton = new JButton("Invoice");
+				tableMenu.add(customerButton);
+				tableMenu.add(productButton);
+				tableMenu.add(invoiceButton);
+
+				JPanel tableOperations = new JPanel(new FlowLayout());
+				add(tableOperations, BorderLayout.PAGE_END);
+				JButton add = new JButton("ADD");
+				JButton edit = new JButton("EDIT");
+				JButton delete = new JButton("DELETE");
+				tableOperations.add(add);
+				tableOperations.add(edit);
+				tableOperations.add(delete);
+				setVisible(true);
+			}
+	}
