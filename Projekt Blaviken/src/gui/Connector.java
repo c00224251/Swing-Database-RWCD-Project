0f1f83eb0b3077ed5bffa8720fd0053fc33a2212
RobertBinkowski <56690174+RobertBinkowski@users.noreply.github.com>
2020-02-08 01:54:06 +0000

    Gui + Connector

---
+++ Projekt Blaviken/src/gui/Connector.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connector {
+
+    private static final String CONN = "jdbc:mysql://localhost/proj3kt?useSSL=false";
+    private static final String USER = "root";
+    private static final String PW = "mysql";
+
+    public static Connection getConnection() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection(CONN, USER, PW);
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("class not found" + e.getMessage());
+            return null;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+}
--- Projekt Blaviken/src/gui/main.java
+++ Projekt Blaviken/src/gui/main.java
@@ -1,6 +1,13 @@
 //package gui;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+// import java.sql.Connection;
+// import java.sql.SQLException;
+
+// import com.mysql.jdbc.PreparedStatement;
 
 /*
 
@@ -9,34 +16,192 @@ import javax.swing.JFrame;
     Date Started:   17/01/2020
     Date Ended:     []/02/2020
 
-    []  To Do: []
-
-    [[] -   Main
-    {}  -   Create Body
-    {}] -   Connect to the server
-    [[] -   Log In
-    {}  -   Add buttons
-    {}  -   Add Text Fields
-    {}  -   Add Labels
-    {{} -   Methods:
-    {}  -   Exit
-    {}}]-   Log In Button
-    [[] -   Main Page
-    {}  -   
-    ]
-
-
  */
-public class main{
+public class main implements ActionListener {
+    JPanel cards; // a panel that uses CardLayout
+    // Main Stuff to Edit
+    final static String projectName = "name";
+    final static int[] minDimension = { 1000, 700 };
+    final static String[] mainMenu = { "Main Menu", "Customer", "Product", "Invoice" };
+    final static String[] buttons = { "Add", "Delete", "Edit" };
+    final static Color color1 = Color.blue; // Set Color of all things
+    final static Color color2 = Color.red; // Set Color of all things
+    final static Color color3 = Color.yellow; // Set Color of all things
+
+    String[] columns = new String[] { "Id", "Name", "Hourly Rate", "Part Time" }; // Columns
+    Object[][] data = new Object[][] { { 1, "John", 40.0, false }, { 2, "Rambo", 70.0, false },
+            { 3, "Zorro", 60.0, true }, };
+
+    JTable customers = new JTable(customersData, customersColumns);
+    JTable products = new JTable(productsData, productsColumns);
+    JTable invoices = new JTable(invoicesData, invoicesColumns);
+
+    // public static Connection connection = Connector.getConnection();
 
     public static void main(String[] args) {
 
-        Body mainPage = new Body("Main Menu");
-        mainPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainPage.setSize(1000, 500);
-        // mainPage.setIconImage(icon);
-        mainPage.setVisible(true);
+        // PreparedStatement pstm = null;
+        // String SQL = "INSERT INTO customer(firstName, secondName, phoneNo, email,
+        // address) VALUES (?,?,?,?,?)";
 
-    }// end main
+        // try {
+        // connection.setAutoCommit(false);
+        // pstm = (PreparedStatement) connection.prepareStatement(SQL);
+        // pstm.setString(1, "John");
+        // pstm.setString(2, "Doe");
+        // pstm.setInt(3, 91282193);
+        // pstm.setString(4, "John.doe@gmail.com");
+        // pstm.setString(5, "1 main street");
+        // connection.commit();
+
+        // } catch (SQLException e) {
+        // e.getMessage();
+        // } finally {
+
+        // try {
+        // // pstm.close();
+        // connection.setAutoCommit(true);
+        // connection.close();
+        // } catch (SQLException ex) {
+        // System.out.println(ex.getMessage());
+        // }
+        // }
+
+        createGui();
+    }// -------------------------------------------------------------------end main
+
+    private JPanel addButtons(JPanel panel) {
+
+        for (int i = 0; i < buttons.length; i++) {
+            JButton button = new JButton(buttons[i]);
+            panel.setBackground(color3);
+            panel.add(button);
+        }
+
+        return panel;
+    }
+
+    private JPanel addContent(JPanel panel, int i) {
+
+        panel.setBackground(color2);
+        if (i == 0) { // Home Page
+
+            panel.setLayout(new GridLayout());
+            panel.add(new JLabel("Hello to our new Project"));
+
+        }
+        if (i == 1) { // Customers
+            JScrollPane pane = new JScrollPane(customers);
+            pane.setBackground(color3);
+            JPanel options = new JPanel();
+            options = addButtons(options);
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+            panel.add(pane, BorderLayout.CENTER);
+            panel.add(options, BorderLayout.PAGE_END);
+        }
+        if (i == 2) { // Products
+            JScrollPane pane = new JScrollPane(products);
+            pane.setBackground(color3);
+            JPanel options = new JPanel();
+            options = addButtons(options);
+
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+            panel.add(pane, BorderLayout.CENTER);
+            panel.add(options, BorderLayout.PAGE_END);
+        }
+        if (i == 3) { // Invoices
+            JScrollPane pane = new JScrollPane(invoices);
+            pane.setBackground(color3);
+            JPanel options = new JPanel();
+            options = addButtons(options);
+
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+            panel.add(pane, BorderLayout.CENTER);
+            panel.add(options, BorderLayout.PAGE_END);
+
+        }
+
+        return panel;
+    }
+
+    private void createScenes(Container pane) {
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        for (int i = 0; i < mainMenu.length; i++) {
+            JButton button = new JButton(mainMenu[i]);
+            button.addActionListener(this);
+            panel.setBackground(color1);
+            panel.add(button, BorderLayout.PAGE_START);
+        }
+        cards = new JPanel(new CardLayout());
+        for (int i = 0; i < mainMenu.length; i++) {
+            JPanel mainPanel = new JPanel();
+            mainPanel = addContent(mainPanel, i);
+            cards.add(mainPanel, mainMenu[i]);
+        }
+
+        pane.add(panel, BorderLayout.PAGE_START);
+        pane.add(cards, BorderLayout.CENTER);
+    }
+
+    private static void createGui() {
+
+        JFrame frame = new JFrame(projectName);// Creat A frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Close uppon close
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);// Start maximised
+        // frame.setIconImage(image);
+
+        // Create and set up the content pane.
+        main demo = new main(); // Start the page
+        demo.createScenes(frame.getContentPane());
+
+        frame.setMinimumSize(new Dimension(minDimension[0], minDimension[1]));
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        CardLayout cl = (CardLayout) (cards.getLayout());
+        cl.show(cards, e.getActionCommand());
+    }
 
 }// end class
+
+// public class Driver {
+
+// public static Connection connection = Connector.getConnection();
+
+// public static void main(String[] args) {
+// // TODO Auto-generated method stub
+
+// PreparedStatement pstm = null;
+// String SQL = "INSERT INTO customer(firstName, secondName, phoneNo, email,
+// address) VALUES (?,?,?,?,?)";
+
+// try {
+// connection.setAutoCommit(false);
+// pstm = (PreparedStatement) connection.prepareStatement(SQL);
+// pstm.setString(1, "John");
+// pstm.setString(2, "Doe");
+// pstm.setInt(3, 91282193);
+// pstm.setString(4, "John.doe@gmail.com");
+// pstm.setString(5, "1 main street");
+// connection.commit();
+
+// } catch (SQLException e) {
+// e.getMessage();
+// } finally {
+
+// try {
+// // pstm.close();
+// connection.setAutoCommit(true);
+// connection.close();
+// } catch (SQLException ex) {
+// System.out.println(ex.getMessage());
+// }
+// }
+
+// }
+
+// }
