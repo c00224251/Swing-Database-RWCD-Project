84264bcdd1d469e791d5c98994e58f7a5ef75c38
RobertBinkowski <56690174+RobertBinkowski@users.noreply.github.com>
2020-03-01 13:12:37 +0000

    Main

--- Projekt Blaviken/src/gui/main.java
+++ Projekt Blaviken/src/gui/main.java
@@ -1,4 +1,4 @@
-package gui;
+//package gui;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
@@ -19,195 +19,154 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-import customer.CustomerData;
-import invoice.InvoiceData;
-import product.ProductData;
+// import customer.CustomerData;
+// import invoice.InvoiceData;
+// import product.ProductData;
 
 /*
 
     By:             
 
     Date Started:   17/01/2020
-    Date Ended:     []/02/2020
+    Due Date:     05/03/2020
 
  */
-public class main implements ActionListener
-	{
-		JPanel cards; // a panel that uses CardLayout
-		// Main Stuff to Edit
-		final static String projectName = "name";
-		final static int[] minDimension = { 1000, 700 };
-		final static String[] mainMenu = { "Main Menu", "Customer", "Product", "Invoice" };
-		final static String[] buttons = { "Add", "Delete", "Edit" };
-		final static Color color1 = Color.blue; // Set Color of all things
-		final static Color color2 = Color.red; // Set Color of all things
-		final static Color color3 = Color.yellow; // Set Color of all things
+public class main implements ActionListener {
+	JPanel cards; // a panel that uses CardLayout
+	// Main Stuff to Edit
+	final static String projectName = "name";
+	final static int[] minDimension = { 1000, 700 };
+	final static String[] mainMenu = { "Main Menu", "Customer", "Product", "Invoice" };
+	final static String[] buttons = { "Add", "Delete", "Edit" };
+	final static Color color1 = Color.blue; // Set Color of all things
+	final static Color color2 = Color.red; // Set Color of all things
+	final static Color color3 = Color.yellow; // Set Color of all things
 
-		// public static Connection connection = Connector.getConnection();
+	// public static Connection connection = Connector.getConnection();
 
-		public static void main(String[] args)
-			{
+	public static void main(String[] args) {
 
-				// Michals connection
-				// PreparedStatement pstm = null;
-				// String SQL = "INSERT INTO customer(firstName, secondName, phoneNo,
-				// email,address) VALUES (?,?,?,?,?)";
+		createGui();
+	}// -------------------------------------------------------------------end main
 
-				// try {
-				// connection.setAutoCommit(false);
-				// pstm = (PreparedStatement) connection.prepareStatement(SQL);
-				// pstm.setString(1, "John");
-				// pstm.setString(2, "Doe");
-				// pstm.setInt(3, 91282193);
-				// pstm.setString(4, "John.doe@gmail.com");
-				// pstm.setString(5, "1 main street");
-				// connection.commit();
+	private JPanel addButtons(JPanel panel, String name) {
 
-				// } catch (SQLException e) {
-				// e.getMessage();
-				// } finally {
+		for (int i = 0; i < buttons.length; i++) {
+			JButton button = new JButton(buttons[i] + " " + name);
+			button.addActionListener(this);
+			panel.setBackground(color3);
+			panel.add(button);
+		}
 
-				// try {
-				// // pstm.close();
-				// connection.setAutoCommit(true);
-				// connection.close();
-				// } catch (SQLException ex) {
-				// System.out.println(ex.getMessage());
-				// }
-				// }
-				createGui();
-			}// -------------------------------------------------------------------end main
+		return panel;
+	}
 
-		private JPanel addButtons(JPanel panel, String name)
-			{
+	private JPanel addContent(JPanel panel, int i) {
 
-				for (int i = 0; i < buttons.length; i++)
-					{
-						JButton button = new JButton(buttons[i] + " " + name);
-						button.addActionListener(this);
-						panel.setBackground(color3);
-						panel.add(button);
-					}
+		// Creating a Table
+		String[] customersColumns = { "ID", "First Name", "Last Name", "Phone Number", "Email", "Address" };
+		// Object[] customersData = CustomerData.customerData();
+		String[] productsColumns = { "ID", "Brand", "Model", "Price", "Description" };
+		// Object[] productsData = ProductData.productData();
+		String[] invoiceColumns = { "ID", "Customer ID", "Product ID", "Date", "Price" };
+		// Object[] invoiceData = InvoiceData.invoiceData();
 
-				return panel;
-			}
+		DefaultTableModel customerTable = new DefaultTableModel(customersColumns, 0);
+		// customerTable.addRow(customersData);
+		DefaultTableModel productTable = new DefaultTableModel(productsColumns, 0);
+		// productTable.addRow(productsData);
+		DefaultTableModel invoiceTable = new DefaultTableModel(invoiceColumns, 0);
+		// invoiceTable.addRow(invoiceData);
 
-		private JPanel addContent(JPanel panel, int i)
-			{
+		JTable customers = new JTable(customerTable);
+		JTable products = new JTable(productTable);
+		JTable invoices = new JTable(invoiceTable);
 
-				// Creating a Table
-				String[] customersColumns = { "ID", "First Name", "Last Name", "Phone Number", "Email", "Address" };
-				Object[] customersData = CustomerData.customerData();
-				String[] productsColumns = { "ID", "Brand", "Model", "Price", "Description" };
-				Object[] productsData = ProductData.productData();
-				String[] invoiceColumns = { "ID", "Customer ID", "Product ID", "Date", "Price" };
-				Object[] invoiceData = InvoiceData.invoiceData();
+		panel.setBackground(color2);
+		if (i == 0) { // Home Page
 
-				DefaultTableModel customerTable = new DefaultTableModel(customersColumns, 0);
-				customerTable.addRow(customersData);
-				DefaultTableModel productTable = new DefaultTableModel(productsColumns, 0);
-				productTable.addRow(productsData);
-				DefaultTableModel invoiceTable = new DefaultTableModel(invoiceColumns, 0);
-				invoiceTable.addRow(invoiceData);
+			panel.setLayout(new GridLayout());
+			panel.add(new JLabel("Hello to our new Project"));
 
-				JTable customers = new JTable(customerTable);
-				JTable products = new JTable(productTable);
-				JTable invoices = new JTable(invoiceTable);
+		}
+		if (i == 1) { // Customers
+			JScrollPane pane = new JScrollPane(customers);
+			pane.setBackground(color3);
+			JPanel options = new JPanel();
+			options = addButtons(options, "Customer");
+			panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+			panel.add(pane, BorderLayout.CENTER);
+			panel.add(options, BorderLayout.PAGE_END);
+		}
+		if (i == 2) { // Products
+			JScrollPane pane = new JScrollPane(products);
+			pane.setBackground(color3);
+			JPanel options = new JPanel();
+			options = addButtons(options, "Product");
 
-				panel.setBackground(color2);
-				if (i == 0)
-					{ // Home Page
+			panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+			panel.add(pane, BorderLayout.CENTER);
+			panel.add(options, BorderLayout.PAGE_END);
+		}
+		if (i == 3) { // Invoices
+			JScrollPane pane = new JScrollPane(invoices);
+			pane.setBackground(color3);
+			JPanel options = new JPanel();
+			options = addButtons(options, "Invoice");
 
-						panel.setLayout(new GridLayout());
-						panel.add(new JLabel("Hello to our new Project"));
+			panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+			panel.add(pane, BorderLayout.CENTER);
+			panel.add(options, BorderLayout.PAGE_END);
 
-					}
-				if (i == 1)
-					{ // Customers
-						JScrollPane pane = new JScrollPane(customers);
-						pane.setBackground(color3);
-						JPanel options = new JPanel();
-						options = addButtons(options, "Customer");
-						panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-						panel.add(pane, BorderLayout.CENTER);
-						panel.add(options, BorderLayout.PAGE_END);
-					}
-				if (i == 2)
-					{ // Products
-						JScrollPane pane = new JScrollPane(products);
-						pane.setBackground(color3);
-						JPanel options = new JPanel();
-						options = addButtons(options, "Product");
+		}
 
-						panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-						panel.add(pane, BorderLayout.CENTER);
-						panel.add(options, BorderLayout.PAGE_END);
-					}
-				if (i == 3)
-					{ // Invoices
-						JScrollPane pane = new JScrollPane(invoices);
-						pane.setBackground(color3);
-						JPanel options = new JPanel();
-						options = addButtons(options, "Invoice");
+		return panel;
+	}
 
-						panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-						panel.add(pane, BorderLayout.CENTER);
-						panel.add(options, BorderLayout.PAGE_END);
+	private void createScenes(Container pane) {
 
-					}
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		for (int i = 0; i < mainMenu.length; i++) {
+			JButton button = new JButton(mainMenu[i]);
+			button.addActionListener(this);
+			panel.setBackground(color1);
+			panel.add(button, BorderLayout.PAGE_START);
+		}
+		cards = new JPanel(new CardLayout());
+		for (int i = 0; i < mainMenu.length; i++) {
+			JPanel mainPanel = new JPanel();
+			mainPanel = addContent(mainPanel, i);
+			cards.add(mainPanel, mainMenu[i]);
+		}
 
-				return panel;
-			}
+		pane.add(panel, BorderLayout.PAGE_START);
+		pane.add(cards, BorderLayout.CENTER);
+	}
 
-		private void createScenes(Container pane)
-			{
+	private static void createGui() {
 
-				JPanel panel = new JPanel();
-				panel.setLayout(new FlowLayout());
-				for (int i = 0; i < mainMenu.length; i++)
-					{
-						JButton button = new JButton(mainMenu[i]);
-						button.addActionListener(this);
-						panel.setBackground(color1);
-						panel.add(button, BorderLayout.PAGE_START);
-					}
-				cards = new JPanel(new CardLayout());
-				for (int i = 0; i < mainMenu.length; i++)
-					{
-						JPanel mainPanel = new JPanel();
-						mainPanel = addContent(mainPanel, i);
-						cards.add(mainPanel, mainMenu[i]);
-					}
+		JFrame frame = new JFrame(projectName);// Create A frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Terminate the program completely on close
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);// Start maximized
+		// frame.setIconImage(image);
 
-				pane.add(panel, BorderLayout.PAGE_START);
-				pane.add(cards, BorderLayout.CENTER);
-			}
+		// Create and set up the content pane.
+		main demo = new main(); // Start the page
+		demo.createScenes(frame.getContentPane());
 
-		private static void createGui()
-			{
+		frame.setMinimumSize(new Dimension(minDimension[0], minDimension[1]));
+		frame.setVisible(true);
+	}
 
-				JFrame frame = new JFrame(projectName);// Create A frame
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Terminate the program completely on close
-				frame.setExtendedState(JFrame.MAXIMIZED_BOTH);// Start maximized
-				// frame.setIconImage(image);
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		CardLayout cl = (CardLayout) (cards.getLayout());
+		cl.show(cards, e.getActionCommand());
 
-				// Create and set up the content pane.
-				main demo = new main(); // Start the page
-				demo.createScenes(frame.getContentPane());
+	}
 
-				frame.setMinimumSize(new Dimension(minDimension[0], minDimension[1]));
-				frame.setVisible(true);
-			}
-
-		@Override
-		public void actionPerformed(ActionEvent e)
-			{
-				CardLayout cl = (CardLayout) (cards.getLayout());
-				cl.show(cards, e.getActionCommand());
-
-			}
-
-	}// end class
+}// end class
 
 // public class Driver {
 
