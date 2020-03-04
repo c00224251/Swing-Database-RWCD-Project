e5d6baa7ce2a233a40ac1a15ade2658c50f77d0d
RobertBinkowski <56690174+RobertBinkowski@users.noreply.github.com>
2020-03-04 12:52:21 +0000

    Gui Updated

---
+++ Projekt Blaviken/src/gui/Gui.java
@@ -0,0 +1,159 @@
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+//import customer.CustomerData;
+//import invoice.InvoiceData;
+//import product.ProductData;
+
+class Gui implements ActionListener {
+
+    JPanel cards; // a panel that uses CardLayout
+    // public static Connection connection = Connector.getConnection();
+    // Main Stuff to Edit
+    final static String projectName = "CA3 Project";
+    final static int[] minDimension = { 500, 200 };
+    final static String[] mainMenu = { "Main Menu", "Customer", "Product", "Invoice" };
+    final static String[] buttons = { "Add", "Delete", "Edit" };
+    final static Color color1 = new Color(100, 50, 10); // Set Color of all things
+    final static Color color2 = new Color(255, 255, 255); // Set Color of all things
+    final static Color color3 = new Color(50, 20, 10); // Set Color of all things
+
+    private JPanel addButtons(JPanel panel, String name) {
+
+        for (int i = 0; i < buttons.length; i++) {
+            JButton button = new JButton(buttons[i] + " " + name);
+            button.addActionListener(this);
+            panel.setBackground(color3);
+            panel.add(button);
+        }
+
+        return panel;
+    }
+
+    private JPanel addContent(JPanel panel, int i) {
+
+        // // Creating a Table
+        try {
+            String[] customersColumns = { "ID", "First Name", "Last Name", "Phone Number", "Email", "Address" };
+            Object[] customersData = CustomerData.customerData();
+            String[] productsColumns = { "ID", "Brand", "Model", "Price", "Description" };
+            Object[] productsData = ProductData.productData();
+            String[] invoiceColumns = { "ID", "Customer ID", "Product ID", "Date", "Price" };
+            Object[] invoiceData = InvoiceData.invoiceData();
+        } catch (Exception exep) {
+            System.out.println("Error:\n" + exep.getMessage());
+        }
+
+        DefaultTableModel customerTable = new DefaultTableModel(customersColumns, 0);
+        // customerTable.addRow(customersData);
+        DefaultTableModel productTable = new DefaultTableModel(productsColumns, 0);
+        // productTable.addRow(productsData);
+        DefaultTableModel invoiceTable = new DefaultTableModel(invoiceColumns, 0);
+        // invoiceTable.addRow(invoiceData);
+
+        JTable customers = new JTable(customerTable);
+        JTable products = new JTable(productTable);
+        JTable invoices = new JTable(invoiceTable);
+
+        panel.setBackground(color2);
+        if (i == 0) { // Home Page
+
+            panel.setLayout(new GridLayout());
+            panel.add(new JLabel("Hello to our new Project"));
+
+        }
+        if (i == 1) { // Customers
+            JScrollPane pane = new JScrollPane(customers);
+            pane.setBackground(color3);
+            JPanel options = new JPanel();
+            options = addButtons(options, "Customer");
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+            panel.add(pane, BorderLayout.CENTER);
+            panel.add(options, BorderLayout.PAGE_END);
+        }
+        if (i == 2) { // Products
+            JScrollPane pane = new JScrollPane(products);
+            pane.setBackground(color3);
+            JPanel options = new JPanel();
+            options = addButtons(options, "Product");
+
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+            panel.add(pane, BorderLayout.CENTER);
+            panel.add(options, BorderLayout.PAGE_END);
+        }
+        if (i == 3) { // Invoices
+            JScrollPane pane = new JScrollPane(invoices);
+            pane.setBackground(color3);
+            JPanel options = new JPanel();
+            options = addButtons(options, "Invoice");
+
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+            panel.add(pane, BorderLayout.CENTER);
+            panel.add(options, BorderLayout.PAGE_END);
+
+        }
+
+        return panel;
+    }
+
+    private void createScenes(Container pane) {
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        for (int i = 0; i < mainMenu.length; i++) {
+            JButton button = new JButton(mainMenu[i]);
+            button.addActionListener(this);
+            panel.setBackground(color1);
+            panel.add(button, BorderLayout.PAGE_START);
+        }
+        cards = new JPanel(new CardLayout());
+        for (int i = 0; i < mainMenu.length; i++) {
+            JPanel mainPanel = new JPanel();
+            mainPanel = addContent(mainPanel, i);
+            cards.add(mainPanel, mainMenu[i]);
+        }
+
+        pane.add(panel, BorderLayout.PAGE_START);
+        pane.add(cards, BorderLayout.CENTER);
+    }
+
+    public static void createGui() {
+
+        JFrame frame = new JFrame(projectName);// Create A frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Terminate the program completely on close
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);// Start maximized
+        // frame.setIconImage(image);
+
+        // Create and set up the content pane.
+        Gui demo = new Gui(); // Start the page
+        demo.createScenes(frame.getContentPane());
+
+        frame.setMinimumSize(new Dimension(minDimension[0], minDimension[1]));
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        CardLayout cl = (CardLayout) (cards.getLayout());
+        cl.show(cards, e.getActionCommand());
+
+    }
+
+}
