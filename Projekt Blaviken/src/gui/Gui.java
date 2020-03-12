7193f440fcab5ea33dbfc5fdc19f85cada39e3bc
RobertBinkowski <56690174+RobertBinkowski@users.noreply.github.com>
2020-03-12 04:41:56 +0000

    someUpdate

--- Projekt Blaviken/src/gui/Gui.java
+++ Projekt Blaviken/src/gui/Gui.java
@@ -1,4 +1,3 @@
-package gui;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
@@ -20,10 +19,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
-
-import customer.CustomerData;
-import invoice.InvoiceData;
-import product.ProductData;
 
 // import customer.CustomerData;
 // import invoice.InvoiceData;
@@ -212,15 +207,11 @@ class Gui implements ActionListener
 				String buttonLabel = e.getActionCommand();
 				if (buttonLabel.equals("Add Customer"))
 					{
-						AddCustomerGui addCustomer = new AddCustomerGui();
-						addCustomer.setVisible(true);
 					} else if (buttonLabel.equals("Delete Customer"))
 					{
 						Popup.information("Works!");
 					} else if (buttonLabel.equals("Edit Customer"))
 					{
-						EditCustomerGui editCustomer = new EditCustomerGui(customers.getSelectedRow());
-						editCustomer.setVisible(true);
 					} else if (buttonLabel.equals("Delete Product"))
 					{
 						Popup.information("Works!");
@@ -242,4 +233,167 @@ class Gui implements ActionListener
 					}
 			}
 
+	}class Gui implements ActionListener {
+
+	JPanel cards; // a panel that uses CardLayout
+	// public static Connection connection = Connector.getConnection();
+	// Main Stuff to Edit
+	final static String projectName = "CA3 Project"; // Project Name
+	final static int[] minDimension = { 500, 200 }; // Set dimensions
+	final static String[] mainMenu = { "Main Menu", "Customer", "Product", "Invoice" };// All the pages
+	final static String[] buttons = { "Add", "Delete", "Edit" };
+	final static Color color1 = new Color(100, 50, 10); // Set Color of all things
+	final static Color color2 = new Color(255, 255, 255); // Set Color of all things
+	final static Color color3 = new Color(50, 20, 10); // Set Color of all things
+	static JTable customers;
+	static JTable products;
+	static JTable invoices;
+	static JScrollPane scrollCustomers = new JScrollPane(customers);
+	static JScrollPane scrollProducts = new JScrollPane(products);
+	static JScrollPane scrollInvoices = new JScrollPane(invoices);
+
+	private JPanel addButtons(JPanel panel, String name) {
+
+		for (int i = 0; i < buttons.length; i++) {
+			JButton button = new JButton(buttons[i] + " " + name);
+			button.addActionListener(this);
+			panel.setBackground(color3);
+			panel.add(button);
+		}
+
+		return panel;
 	}
+
+	private JPanel addContent(JPanel panel, int i) {
+
+		// // Creating a Table
+
+		String[] customersColumns = { "ID", "First Name", "Last Name", "Phone Number", "Email", "Address" };
+		Object[] productsColumns = { "ID", "Brand", "Model", "Price", "Description" };
+		Object[] invoicesColumns = { "ID", "Customer ID", "Product ID", "Date", "Price" };
+
+		// customers.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		// products.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		// invoices.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		// customers.addMouseListener(new MouseAdapter() {
+		// public void mouseClicked(MouseEvent e) {
+		// System.out.println(customers.getSelectedRow());
+		// }
+
+		// });
+
+		// products.addMouseListener(new MouseAdapter() {
+		// public void mouseClicked(MouseEvent e) {
+		// System.out.println(products.getSelectedRow());
+		// }
+
+		// });
+
+		panel.setBackground(color2);
+		if (i == 0) { // Home Page
+			String welcome;
+			welcome = "\tCA3";
+			welcome += "\t\nBy:";
+			welcome += "\t\nRobert Binkowski - C00237917";
+			welcome += "\t\n";
+			welcome += "\t\n";
+
+			panel.setLayout(new GridLayout());
+			panel.add(new JLabel(welcome));
+
+		}
+		if (i == 1) { // Customers
+			scrollCustomers.setBackground(color3);
+			JPanel options = new JPanel();
+			options = addButtons(options, "Customer");
+			panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+			panel.add(scrollCustomers, BorderLayout.CENTER);
+			panel.add(options, BorderLayout.PAGE_END);
+		}
+		if (i == 2) { // Products
+			scrollProducts.setBackground(color3);
+			JPanel options = new JPanel();
+			options = addButtons(options, "Product");
+
+			panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+			panel.add(scrollProducts, BorderLayout.CENTER);
+			panel.add(options, BorderLayout.PAGE_END);
+		}
+		if (i == 3) { // Invoices
+			scrollInvoices.setBackground(color3);
+			JPanel options = new JPanel();
+			options = addButtons(options, "Invoice");
+			panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+			panel.add(scrollInvoices, BorderLayout.CENTER);
+			panel.add(options, BorderLayout.PAGE_END);
+
+		}
+
+		return panel;
+	}
+
+	private void createScenes(Container pane) {
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		for (int i = 0; i < mainMenu.length; i++) {
+			JButton button = new JButton(mainMenu[i]);
+			button.addActionListener(this);
+			panel.setBackground(color1);
+			panel.add(button, BorderLayout.PAGE_START);
+		}
+		cards = new JPanel(new CardLayout());
+		for (int i = 0; i < mainMenu.length; i++) {
+			JPanel mainPanel = new JPanel();
+			mainPanel = addContent(mainPanel, i);
+			cards.add(mainPanel, mainMenu[i]);
+		}
+
+		pane.add(panel, BorderLayout.PAGE_START);
+		pane.add(cards, BorderLayout.CENTER);
+	}
+
+	public static void createGui() {
+
+		JFrame frame = new JFrame(projectName);// Create A frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Terminate the program completely on close
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);// Start maximized
+		// frame.setIconImage(image);
+
+		// Create and set up the content pane.
+		Gui demo = new Gui(); // Start the page
+		demo.createScenes(frame.getContentPane());
+
+		frame.setMinimumSize(new Dimension(minDimension[0], minDimension[1]));
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		CardLayout cl = (CardLayout) (cards.getLayout());
+		cl.show(cards, e.getActionCommand());
+
+		String buttonLabel = e.getActionCommand();
+		if (buttonLabel.equals("Add Customer")) {
+
+		} else if (buttonLabel.equals("Delete Customer")) {
+			Popup.information("Works!");
+		} else if (buttonLabel.equals("Edit Customer")) {
+
+		} else if (buttonLabel.equals("Delete Product")) {
+			Popup.information("Works!");
+		} else if (buttonLabel.equals("Edit Product")) {
+			Popup.information("Works!");
+		} else if (buttonLabel.equals("Add Product")) {
+			Popup.information("Works!");
+		} else if (buttonLabel.equals("Add Invoice")) {
+			Popup.information("Works!");
+		} else if (buttonLabel.equals("Delete Invoice")) {
+			Popup.information("Works!");
+		} else if (buttonLabel.equals("Edit Invoice")) {
+			Popup.information("Works!");
+		}
+	}
+
+}
--- Projekt Blaviken/src/gui/main.java
+++ Projekt Blaviken/src/gui/main.java
@@ -1,4 +1,7 @@
 package gui;
+//package gui;
+
+//import gui.Popup;
 
 /*
 
@@ -10,10 +13,19 @@ package gui;
  */
 public class main
 	{
+public class main {
+
+	public static void main(String[] args) {
 
 		public static void main(String[] args)
 			{
 				Gui.createGui();
 			}// -------------------------------------------------------------------end main
+		try {
+			Gui.createGui();
+		} catch (Exception exp) {
+			Popup.error("Gui failed to load:\n" + exp.toString());
+		}
+	}// -------------------------------------------------------------------end main
 
-	}// end class
+}// end class
